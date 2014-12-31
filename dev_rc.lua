ce730a38c1d52db15bf7aee12a489c60afe5048a
Matt McKellar-Spence <matt@mattms.com>
2014-12-31 21:58:10 +1100

    Switch notification arguments

--- dev_rc.lua
+++ dev_rc.lua
@@ -8,8 +8,8 @@ require("naughty")
 if awesome.startup_errors then
 	naughty.notify {
 		preset = naughty.config.presets.critical,
-		title = "There were errors during startup!",
-		text = awesome.startup_errors
+		text = awesome.startup_errors,
+		title = "There were errors during startup!"
 	}
 end
 
